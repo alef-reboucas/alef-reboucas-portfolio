d04abacbfc34a9f9a63bb14996059d5f6720bf1d
alef-reboucas <alefreboucas@gmail.com>
2025-04-16 15:14:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // Aqui vai a alteração
+  base: '/alef-reboucas-portfolio/', // Altere para o nome do seu repositório no GitHub
 }));
